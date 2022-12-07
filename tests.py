a011b9baa47079b165dbd627747a42de951dcf69
sinkaroid <bibsindra@gmail.com>
2022-12-07 09:45:20 +0700

    ci: plenty of testing cases

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import argparse
+import aiohttp
 import asyncio
 import booru
-import requests
 import os
 
 
@@ -29,7 +29,7 @@ class Wrapper(object):
         self.paheal = booru.Paheal()
 
     @staticmethod
-    def fetch(imgboard: str, search: str):
+    async def fetch(imgboard: str, search: str):
         """Fetches the data from safebooru.
 
         Parameters
@@ -46,11 +46,11 @@ class Wrapper(object):
             The data that represents the search from separate booru
         """
 
-        async def main():
-            data = await imgboard.search(query=search, limit=5)
-            print(data)
+        data = await imgboard.search(query=search)
+        print("Data", data)
 
-        return asyncio.run(main())
+        image = await imgboard.search_image(query=search)
+        print("Image", image)
 
 
 Base = Wrapper()
@@ -84,45 +84,45 @@ parse.add_argument("-changelog", action="store_true")
 args = parse.parse_args()
 
 
-def main():
+async def main():
     if args.gelbooru:
-        Wrapper.fetch(Base.gelbooru, args.gelbooru)
+        await Wrapper.fetch(Base.gelbooru, args.gelbooru)
     elif args.rule34:
-        Wrapper.fetch(Base.rule34, args.rule34)
+        await Wrapper.fetch(Base.rule34, args.rule34)
     elif args.tbib:
-        Wrapper.fetch(Base.tbib, args.tbib)
+        await Wrapper.fetch(Base.tbib, args.tbib)
     elif args.safebooru:
-        Wrapper.fetch(Base.safebooru, args.safebooru)
+        await Wrapper.fetch(Base.safebooru, args.safebooru)
     elif args.xbooru:
-        Wrapper.fetch(Base.xbooru, args.xbooru)
+        await Wrapper.fetch(Base.xbooru, args.xbooru)
     elif args.realbooru:
-        Wrapper.fetch(Base.realbooru, args.realbooru)
+        await Wrapper.fetch(Base.realbooru, args.realbooru)
     elif args.hypnohub:
-        Wrapper.fetch(Base.hypnohub, args.hypnohub)
+        await Wrapper.fetch(Base.hypnohub, args.hypnohub)
     elif args.danbooru:
-        Wrapper.fetch(Base.danbooru, args.danbooru)
+        await Wrapper.fetch(Base.danbooru, args.danbooru)
     elif args.atfbooru:
-        Wrapper.fetch(Base.atfbooru, args.atfbooru)
+        await Wrapper.fetch(Base.atfbooru, args.atfbooru)
     elif args.yandere:
-        Wrapper.fetch(Base.yandere, args.yandere)
+        await Wrapper.fetch(Base.yandere, args.yandere)
     elif args.konachan:
-        Wrapper.fetch(Base.konachan, args.konachan)
+        await Wrapper.fetch(Base.konachan, args.konachan)
     elif args.konachan_net:
-        Wrapper.fetch(Base.konachan_net, args.konachan_net)
+        await Wrapper.fetch(Base.konachan_net, args.konachan_net)
     elif args.lolibooru:
-        Wrapper.fetch(Base.lolibooru, args.lolibooru)
+        await Wrapper.fetch(Base.lolibooru, args.lolibooru)
     elif args.e6:
-        Wrapper.fetch(Base.e621, args.e6)
+        await Wrapper.fetch(Base.e621, args.e6)
     elif args.e9:
-        Wrapper.fetch(Base.e926, args.e9)
+        await Wrapper.fetch(Base.e926, args.e9)
     elif args.derpibooru:
-        Wrapper.fetch(Base.derpibooru, args.derpibooru)
+        await Wrapper.fetch(Base.derpibooru, args.derpibooru)
     elif args.furbooru:
-        Wrapper.fetch(Base.furbooru, args.furbooru)
+        await Wrapper.fetch(Base.furbooru, args.furbooru)
     elif args.behoimi:
-        Wrapper.fetch(Base.behoimi, args.behoimi)
+        await Wrapper.fetch(Base.behoimi, args.behoimi)
     elif args.paheal:
-        Wrapper.fetch(Base.paheal, args.paheal)
+        await Wrapper.fetch(Base.paheal, args.paheal)
     elif args.build:
         print(booru.__version__)
 
@@ -136,12 +136,13 @@ def main():
             if res == "http://behoimi.org":
                 Internal.headers = Internal.behoimi_bypass
 
-            r = requests.get(res, headers=Internal.headers)
-            print(res, r.status_code)
+            async with aiohttp.ClientSession(headers=Internal.headers) as resp:
+                async with resp.get(res) as data:
+                    print(f"{api} : {data.status}")
 
     else:
         print("No arguments given")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
